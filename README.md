160b122bcb46bc3c12df1ca5d3afba778fede719
Mark Raynsford <code@io7m.com>
2018-03-25 10:06:19 +0000

    Rename project and adopt new conventions
    Use the new primogenitor POM and 2017 project conventions.
    
    See http://blog.io7m.com/2017/02/25/java-module-renaming.xhtml

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 gtyrell
 ===
 
-[![Build Status](https://travis-ci.org/io7m/gtyrell.svg)](https://travis-ci.org/io7m/gtyrell)
+[![Build Status](https://travis-ci.org/io7m/gtyrell.png)](https://travis-ci.org/io7m/gtyrell)
 [![Maven Central](https://maven-badges.herokuapp.com/maven-central/com.io7m.gtyrell/io7m-gtyrell/badge.png)](https://maven-badges.herokuapp.com/maven-central/com.io7m.gtyrell/io7m-gtyrell)
 [![Codacy Badge](https://api.codacy.com/project/badge/Grade/1a59ea6bf43c4f5896a3b0195037be64)](https://www.codacy.com/app/github_79/gtyrell?utm_source=github.com&amp;utm_medium=referral&amp;utm_content=io7m/gtyrell&amp;utm_campaign=Badge_Grade)
 
---
+++ com.io7m.gtyrell.core/src/main/java/com/io7m/gtyrell/core/GTProcessUtilities.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright © 2017 <code@io7m.com> http://io7m.com
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+ * SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR
+ * IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+package com.io7m.gtyrell.core;
+
+import com.io7m.junreachable.UnreachableCodeException;
+import org.apache.commons.io.IOUtils;
+import org.slf4j.Logger;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+final class GTProcessUtilities
+{
+  private GTProcessUtilities()
+  {
+    throw new UnreachableCodeException();
+  }
+
+  static void executeLogged(
+    final Logger log,
+    final Process p,
+    final List<String> out_lines)
+    throws IOException
+  {
+    try (InputStream p_stdout = p.getInputStream()) {
+      try (BufferedReader r_stdout = new BufferedReader(
+        new InputStreamReader(p_stdout, StandardCharsets.UTF_8))) {
+
+        while (true) {
+          final String out_line = r_stdout.readLine();
+          if (out_line == null) {
+            break;
+          }
+
+          out_lines.add(out_line);
+          log.debug("execute: {}", out_line);
+        }
+      }
+
+      try {
+        p.waitFor();
+      } catch (final InterruptedException e) {
+        log.error(
+          "interrupted whilst waiting for process: ", e);
+      }
+
+      if (p.exitValue() > 0) {
+        try (ByteArrayOutputStream bao = new ByteArrayOutputStream()) {
+          IOUtils.writeLines(out_lines, "\n", bao, StandardCharsets.UTF_8);
+          throw new IOException(bao.toString("UTF-8"));
+        }
+      }
+    }
+  }
+
+}
